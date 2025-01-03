733b5ee9ee2ddc68c5821be8564b4a0251d41259
Madhumithach <157323201+Madhumithach@users.noreply.github.com>
2025-01-03 17:51:35 +0000

    class06 STring operations

---
+++ 02_Basics/hex_to_oct_conversion.py
@@ -0,0 +1,10 @@
+#hexadecimal to octal number conversion
+hexa="1F4"
+decimal_number=int(hexa,16)
+octal_number=oct(decimal_number)
+print(octal_number)
+#octal to hexadecimal conversion
+octal= "764"
+decimal_number_o=int(octal,8)
+hexadecimal_number=hex(decimal_number_o)
+print(hexadecimal_number)
